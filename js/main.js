b9ba4f35c5682a3d744fbef4c32c37a33d3c5a04
Sawera-Dahri <sayeradahri@gmail.com>
2026-01-07 15:26:27 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//active- navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop  > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+//nav-hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse');
+
+navBar.forEach(function(a) {
+    a.addEventListener("click", function() {
+        navCollapse.classList.remove("show");
+    });
+});
+//counter design//
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range));
+
+    let timer = setInterval(() => {
+      current += increment;
+      obj.textContent = current;
+      if (current == end) {
+        clearInterval(timer);
+      }
+    }, step);
+  }
+
+  counter("count-1", 2690, 1234, 3000);
+  counter("count-2", 8782, 1065, 1683);
+  counter("count-3", 6289, 2584, 4567);
+  counter("count-4", 3834, 3456, 6543);
+});
